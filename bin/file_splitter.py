b506dc1814e427ec0abcad77fec68e46840fbe70
Joshua Westgard <jwestgard@mac.com>
2024-04-03 17:13:05 -0400

    read METS to build file list

--- bin/file_splitter.py
+++ bin/file_splitter.py
@@ -12,7 +12,9 @@ class ArchelonBatchCsv:
     def __init__(self, path):
         self.path = path
         with open(self.path, 'r') as handle:
-            self.data = [row for row in csv.DictReader(handle)]
+            reader = csv.DictReader(handle)
+            self.data = [row for row in reader]
+            self.fieldnames = reader.fieldnames
 
     def add_files_column(self, filelist):
         for row in self.data:
@@ -29,71 +31,19 @@ class ArchelonBatchCsv:
         return output
 
     def write(self):
-        fieldnames = list(set().union(*[d.keys() for d in self.data]))
-        writer = csv.DictWriter(sys.stdout, fieldnames=fieldnames)
+        writer = csv.DictWriter(sys.stdout, fieldnames=self.fieldnames)
         writer.writeheader()
         for row in self.data:
             writer.writerow(row)
 
 
-class Component:
+class MigrationDbCsv:
 
-    def __init__(self, sequence, pid):
-        pass
+    def __init__(self, path):
+        self.path = path
+        self.data = []
 
-
-class ImageAsset:
-
-    def __init__(self, n, path):
-        self.seq = n
-        self.path = pathlib.Path(path)
-        self.umdm = self.path.parts[0]
-        self.umam = self.path.parts[1].replace('_', ':')
-        self.filename = self.path.name
-        self.base = self.path.stem
-        self.ext  = self.path.suffix.replace('.','').upper()
-        #print(self.umdm, self.umam, self.base, self.ext)
-
-    def __lt__(self, other): 
-        return self.seq < other.seq
-
-
-def get_page_sort(path):
-
-    ns = {"mets": "http://www.loc.gov/METS/"}
-    filesec = '//mets:mets/mets:fileSec/mets:fileGrp/mets:file'
-    structmap = '//mets:mets/mets:structMap/mets:div[@ID="images"]'
-    file_to_pid = {}
-    results = []
-
-    with open(path) as handle:
-        tree = etree.parse(handle)
-
-    for node in tree.xpath(filesec, namespaces=ns):
-        fileid = node.get('ID')
-        pid = node.xpath('mets:FLocat', namespaces=ns)[0].get(
-            '{http://www.w3.org/1999/xlink}href'
-            )
-        file_to_pid[fileid] = pid
-
-    for node in tree.xpath(structmap, namespaces=ns):
-        for child in node:
-            seq = int(child.get('ORDER'))
-            label = child.get('LABEL')
-            fileid = child.xpath('mets:div/mets:fptr', namespaces=ns)[0].get('FILEID')
-            results.append((seq, label, file_to_pid[fileid]))
-
-    return sorted(results)
-
-
-def main():
-
-    inputcsv = ArchelonBatchCsv(sys.argv[1])
-    fieldnames = ['umdm', 'seq', 'umam', 'basename', 'ext', 'path']
-    with open('output.csv', 'w+') as handle:
-        writer = csv.DictWriter(handle, fieldnames=fieldnames)
-        writer.writeheader()
-
+        """
         for row in inputcsv.data:
             structure = StructMap(row['umdm'])
             filelist = [f for f in row['FILES'].split('|') if f != ""]
@@ -121,7 +71,119 @@ def main():
                     'ext':      f.ext,
                     'path':     f.path
                     })
+        """
+
+    def write(self):
+        fieldnames = ['umdm', 'seq', 'umam', 'basename', 'ext', 'path']
+        with open(self.path, 'w+') as handle:
+            writer = csv.DictWriter(handle, fieldnames=fieldnames)
+        writer.writeheader()
+        for row in self.data:
+            writer.writerow(row)
+
+
+class Item:
+
+    registry = {}
+
+    def __init__(self, pid):
+        self.pid = pid
+        self.pages = []
+
+    @classmethod
+    def create(cls, pid):
+        try:
+            return cls.registry[pid]
+        except KeyError:
+            new = cls(pid)
+            cls.registry[pid] = new
+            return new
+
+    def parse_mets_record(self, path):
+        ns = {"mets": "http://www.loc.gov/METS/"}
+        filesec = '//mets:mets/mets:fileSec/mets:fileGrp/mets:file'
+        structmap = '//mets:mets/mets:structMap/mets:div[@ID="images"]'
+        file2pid = {}
+
+        with open(path) as handle:
+            tree = etree.parse(handle)
+
+        for node in tree.xpath(filesec, namespaces=ns):
+            fileid = node.get('ID')
+            child = node.xpath('mets:FLocat', namespaces=ns)[0]
+            pid = child.get('{http://www.w3.org/1999/xlink}href')
+            file2pid[fileid] = pid
+
+        for node in tree.xpath(structmap, namespaces=ns):
+            for child in node:
+                seq = int(child.get('ORDER'))
+                label = child.get('LABEL')
+                fileid = child.xpath('mets:div/mets:fptr', namespaces=ns)[0].get('FILEID')
+                self.pages.append(
+                    Component.create(seq, file2pid[fileid], label)
+                    )
+        self.pages.sort()
+
+    def tree(self):
+        print(self.pid)
+        for page in sorted([p for p in self.pages]):
+            print(f"  ({page.seq}) {page.pid}")
+            for file in sorted([f for f in page.files]):
+                print(f"    • {file.filename}")
+
+
+class Component:
+    registry = {}
+
+    def __init__(self, seq, pid, label):
+        self.pid = pid
+        self.seq = seq
+        self.files = set()
+
+    @classmethod
+    def create(cls, seq, pid, label):
+        try:
+            return cls.registry[pid]
+        except KeyError:
+            new = cls(seq, pid, label)
+            cls.registry[pid] = new
+            return new
+
+    def __lt__(self, other):
+        return self.seq < other.seq
+
+
+class File:
+
+    def __init__(self, n, path):
+        self.seq = n
+        self.path = pathlib.Path(path)
+        self.umdm = self.path.parts[0]
+        self.umam = self.path.parts[1].replace('_', ':')
+        self.filename = self.path.name
+        self.base = self.path.stem
+        self.ext  = self.path.suffix.replace('.','').upper()
+        #print(self.umdm, self.umam, self.base, self.ext)
+
+    def __lt__(self, other): 
+        return self.seq < other.seq
+
+
+
+
+
+def main():
+
+    inputcsv = ArchelonBatchCsv(sys.argv[1])
+    file_storage_root = sys.argv[2]
+
+    for row in inputcsv.data:
+        item = Item(row['Identifier'])
+        umdm_dir = item.pid.replace(':', '_')
+        item.parse_mets_record(f"{file_storage_root}/{umdm_dir}/foxml.xml")
+        row['FILES'] = "|".join([page.pid for page in item.pages])
+        inputcsv.write()
 
 
 if __name__ == "__main__":
-    print(get_page_sort(sys.argv[1]))
+    main()
